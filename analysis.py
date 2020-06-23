905a7a0c1bbdd732c083c7e4e26e8cf9e3a8e560
Niteshrocks <niteshyadav585@gmail.com>
2020-06-23 22:19:31 +0530

    Add file

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+#Used to convert the raw text file into structured csv file
 import os
 import re
 import pandas as pd
@@ -15,6 +16,7 @@ def analysis(filepath,filename):
 
     split = text.split('\n')
     for s in split:
+	  # regular expression to get the data, number and name in one go
         if(re.match('[0-9]+/[0-9]+/[0-9]+, [0-9]+:[0-9]+ [a-zA-Z]',s)):
             d = s.find(',')
             h = s.find('-')
@@ -31,13 +33,15 @@ def analysis(filepath,filename):
                    "Contacts" : contact,
                    "Messages" :msgs,
                    "Shift":shift})
-    
+
+    # Stopwords that should be removed from our text to make it clean
     stopwords = ['removed','left','added','lef','changed','was','joined','Messages','created',
                 'were','group']
     
     for word in stopwords:
         df = df[~df['Contacts'].str.contains(word)]
     
+    # remove extra space from front and back of the feature
     df['Date'] = df['Date'].str.strip()
     df['Time'] = df['Time'].str.strip()
     df['Contacts'] = df['Contacts'].str.strip()
@@ -46,8 +50,10 @@ def analysis(filepath,filename):
 
     df['Date'] = pd.to_datetime(df['Date'])
     
+    # change the format of date 
     df['Date'] = df['Date'].dt.strftime('%d/%m/%Y')
     
     df.index = [ i for i in range(1,len(df)+1)]
+    # Save the file as csv on the server
     df.to_csv(os.path.join('csvs',filename),index=False)
     return 
